15f8664e2c10f6d65c14c729394f263de71f1327
Chiamaka Ugwu <chiamakaugwu.m@gmail.com>
2025-04-03 20:16:14 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const changeImage1 = document.getElementById("image1");
+const changeImage2 = document.getElementById("image2");
+const changeImage3 = document.getElementById("image3");
+const changeImage4 = document.getElementById("image4");
+const changeImage5 = document.getElementById("image5");
+const changeImage6 = document.getElementById("image6");
+const changeImage7 = document.getElementById("image7");
+const changeImage8 = document.getElementById("image8");
+const changeImage9 = document.getElementById("image9");
+
+changeImage1.addEventListener("click", function () {
+  changeImage1.style.backgroundColor = "green";
+  changeImage1.style.color = "white";
+  changeImage1.innerHTML = "These flowers bloom all summer long and into fall.";
+});
+
+changeImage2.addEventListener("click", function () {
+  changeImage2.style.backgroundColor = "purple";
+  changeImage2.style.color = "white";
+  changeImage2.innerHTML =
+    "This evergreen shrub works well as a border in gardens.";
+});
+
+changeImage3.addEventListener("click", function () {
+  changeImage3.style.backgroundColor = "burlywood";
+  changeImage3.style.color = "white";
+  changeImage3.innerHTML =
+    "As an aquatic perennial, these flowers grow in ponds or natural pools.";
+});
+
+changeImage4.addEventListener("click", function () {
+  changeImage4.style.backgroundColor = "blue";
+  changeImage4.style.color = "white";
+  changeImage4.innerHTML = "Another common name for this flower is Cornflower.";
+});
+
+changeImage5.addEventListener("click", function () {
+  changeImage5.style.backgroundColor = "orange";
+  changeImage5.style.color = "white";
+  changeImage5.innerHTML =
+    "One of the more popular options on our list is the Begonia, which comes in many colors and sizes.";
+});
+
+changeImage6.addEventListener("click", function () {
+  changeImage6.style.backgroundColor = "mediumvioletred";
+  changeImage6.style.color = "white";
+  changeImage6.innerHTML =
+    "Camellia blooms tend to last through winter, providing color to your garden.";
+});
+
+changeImage7.addEventListener("click", function () {
+  changeImage7.style.backgroundColor = "magenta";
+  changeImage7.style.color = "white";
+  changeImage7.innerHTML =
+    "Candytufts come in white most often, but they also come in a few other bright colors.";
+});
+
+changeImage8.addEventListener("click", function () {
+  changeImage8.style.backgroundColor = "blueviolet";
+  changeImage8.style.color = "white";
+  changeImage8.innerHTML =
+    "Lavender is one of the most popular flowers for its fragrance, so plant it where you can enjoy it.";
+});
+
+changeImage9.addEventListener("click", function () {
+  changeImage9.style.backgroundColor = "salmon";
+  changeImage9.style.color = "white";
+  changeImage9.innerHTML =
+    "These gorgeous flowers grow well in gardens but also work amazingly as cut flowers.";
+});
